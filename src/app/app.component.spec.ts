0995f7f49918e7c33e64fd65f1aea9d95c762693
Michael Oguidan <michaeloguidan@gmail.com>
2024-01-04 13:46:17 +0000

    Update project name and titles

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ap57-frontend' title`, () => {
+  it(`should have the 'aq54-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ap57-frontend');
+    expect(app.title).toEqual('aq54-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ap57-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, aq54-frontend');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ap57-frontend';
+  title = 'aq54-frontend';
 }
