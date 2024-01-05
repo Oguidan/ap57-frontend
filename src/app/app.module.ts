83b5b14ab73f151dbbe69cc3c8e63afdc7db2545
Michael Oguidan <michaeloguidan@gmail.com>
2024-01-05 13:19:27 +0000

    Update user authentication logic

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+// app.module.ts
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';  // Import CommonModule
+
+import { AppComponent } from './app.component';
+import { DataService } from './services/data.service';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,  // Add CommonModule to the imports array
+  ],
+  providers: [DataService],
+})
+export class AppModule { }
