68769c60e74e44ca0258e5d09d38ad03097c63f8
Michael Oguidan <michaeloguidan@gmail.com>
2024-01-11 18:29:49 +0000

    Refactor get-range component HTML and TypeScript files

--- src/app/components/get-range/get-range.component.ts
+++ src/app/components/get-range/get-range.component.ts
@@ -1,5 +1,6 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-get-range',
@@ -9,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './get-range.component.css'
 })
 export class GetRangeComponent {
+  RangeData: any;
+
+  station_name = "SMART188";
+  dt_from_string = "2024-01-05";
+  dt_to_string = "2024-01-06";
+
+  constructor(private dataService: DataService) { }
   
+  ngOnInit(): void {
+    this.dataService.getRange(this.station_name, this.dt_from_string, this.dt_to_string).subscribe(
+      (data) => {
+        this.RangeData = data;
+        console.log(this.RangeData);
+      }
+    )
+  }
 }
