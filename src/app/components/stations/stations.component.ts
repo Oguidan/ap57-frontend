357ac42544568084563e5da2f49138128d3fc69c
Michael Oguidan <michaeloguidan@gmail.com>
2024-01-14 20:04:10 +0000

    Add table and populate with airquinoData

--- src/app/components/stations/stations.component.ts
+++ src/app/components/stations/stations.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-stations',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './stations.component.html',
   styleUrl: './stations.component.css'
 })
 export class StationsComponent {
+  airquinoData: any;
 
+  constructor(private dataservice: DataService) {}
+
+  ngOnInit(): void {
+    const projectName = 'AQ54';
+    this.dataservice.getStations(projectName).subscribe((data) => {
+      this.airquinoData = data;
+    })
+  }
 }
