c3f5623e088d6e1575fe4350b6fdc535d802c27d
Michael Oguidan <michaeloguidan@gmail.com>
2024-01-14 16:23:38 +0000

    Add table and data display in get-single-day component

--- src/app/components/get-single-day/get-single-day.component.ts
+++ src/app/components/get-single-day/get-single-day.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-get-single-day',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './get-single-day.component.html',
   styleUrl: './get-single-day.component.css'
 })
 export class GetSingleDayComponent {
+  airquinoData: any;
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    const stationName = 'SMART188';
+    const dtFromString = '2024-01-14'
+    this.dataService.getSingleDay(stationName, dtFromString).subscribe((data) => {
+      this.airquinoData = data;
+    })
+  }
 }
